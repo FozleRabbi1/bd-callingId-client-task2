05417c8436cad9aa6b4afad514f2c345ed06256f
Fozle Rabbi <fozlerabbi9790@gmail.com>
2024-08-14 09:55:00 +0600

    set testimonial button

--- src/Components/Pages/Home/Testimuniyal/Testimonial.jsx
+++ src/Components/Pages/Home/Testimuniyal/Testimonial.jsx
@@ -5,55 +5,73 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 import { Navigation } from "swiper/modules";
+import { useRef } from "react";
+import { IoMdArrowBack, IoMdArrowForward } from "react-icons/io";
 
 const Testimonial = () => {
+    const swiperRef = useRef(null);
 
     const carasodDatas = [
         { id: 1, name: "MARIA D. HALK", role: "MANAGING DIRECTOR", description: "When we talk about Alts, we do not mean a typical business partner, but rather a team that collaborates with us daily, always there for us when we encounter difficulties and celebrate achievements. We see in Alts our best ally for success!" },
         { id: 2, name: "MARIA D. HALK", role: "MANAGING DIRECTOR", description: "When we talk about Alts, we do not mean a typical business partner, but rather a team that collaborates with us daily, always there for us when we encounter difficulties and celebrate achievements. We see in Alts our best ally for success!" },
         { id: 3, name: "MARIA D. HALK", role: "MANAGING DIRECTOR", description: "When we talk about Alts, we do not mean a typical business partner, but rather a team that collaborates with us daily, always there for us when we encounter difficulties and celebrate achievements. We see in Alts our best ally for success!" },
         { id: 4, name: "MARIA D. HALK", role: "MANAGING DIRECTOR", description: "When we talk about Alts, we do not mean a typical business partner, but rather a team that collaborates with us daily, always there for us when we encounter difficulties and celebrate achievements. We see in Alts our best ally for success!" },
+
     ]
 
 
     return (
         <div className="relative py-5 ">
-            <div className="mb-14  overflow-hidden relative h-[150vh] flex justify-center items-center">
-                <Swiper
-                    spaceBetween={30}
-                    centeredSlides={true}
-                    autoplay={{
-                        delay: 4000,
-                        disableOnInteraction: false,
-                    }}
-                    pagination={{
-                        clickable: true,
-                    }}
-                    navigation={true}
-                    speed={1500}
-                    modules={[Navigation]}
-                    className="mySwiper   w-[51%] "
-                >
+            <div className="mb-14  overflow-hidden  h-[150vh] flex justify-center items-center">
 
-                    {
-                        carasodDatas.map(item => (
+
+                <div className="w-[50%]  relative">
+                    <Swiper
+                        spaceBetween={30}
+                        centeredSlides={true}
+                        pagination={{
+                            clickable: true,
+                        }}
+                        navigation={false}
+                        speed={2500}
+                        loop={true}
+                        modules={[Navigation]}
+                        className="mySwiper  "
+                        onSwiper={(swiper) => {
+                            swiperRef.current = swiper;
+                        }}
+                    >
+                        {carasodDatas.map((item) => (
                             <SwiperSlide key={item.id} className="overflow-hidden flex justify-center items-center text-center">
-
-                                <div className="">
+                                <div>
                                     <img className="size-16 mx-auto mb-10" src="https://i.ibb.co/ZHw23pN/comma-removebg-preview.png" alt="" />
                                     <div>
-                                        <p className="text-xl leading-8 italic text-gray-600">{item.description}</p>
+                                        <p className="text-xl leading-8 italic text-black ">{item.description}</p>
                                     </div>
                                     <h2 className="mt-10 text-black font-semibold text-2xl">{item.name}</h2>
                                     <h2 className="mt-1 text-sm text-gray-500">{item.role}</h2>
-
                                 </div>
                             </SwiperSlide>
-                        ))
-                    }
+                        ))}
+                    </Swiper>
 
+                    <div className="flex justify-center mt-4 z-40 absolute -bottom-20 left-1/2 -translate-x-1/2">
 
-                </Swiper>
+                        <button
+                            onClick={() => swiperRef.current.slideNext()}
+                            className="size-12 border border-black duration-300 flex justify-center items-center rounded-full mr-2 text-xl bg-white hover:bg-black text-black hover:text-white"
+                        >
+                            <IoMdArrowBack />
+                        </button>
+                        <button
+                            onClick={() => swiperRef.current.slidePrev()}
+                            className="size-12 border border-black duration-300 flex justify-center items-center rounded-full mr-4 text-xl bg-white hover:bg-black text-black hover:text-white "
+                        >
+                            <IoMdArrowForward />
+                        </button>
+
+                    </div>
+                </div>
 
 
             </div>
@@ -78,7 +96,7 @@ export default Testimonial;
 
 
 
-
+// ==========================================================>>>>> Just for testing
 // import { Swiper, SwiperSlide } from "swiper/react";
 // import "swiper/css";
 // import "swiper/css/pagination";
